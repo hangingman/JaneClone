b67817ae330e5b26a1dfe3abb7c353982d23da67
Hiroyuki Nagata <newserver002@gmail.com>
2012-01-09 17:05:28 +0900

    データ型定義ファイルを忘れていたので追加

---
+++ include/DataType.h
@@ -0,0 +1,20 @@
+/*
+ * DataType.h
+ *
+ *  Created on: 2012/01/05
+ *      Author: learning
+ */
+
+#ifndef DATATYPE_H_
+#define DATATYPE_H_
+
+// wxHashMap用のクラス -- 2chの板名とURLを対応させる
+class URLvsBoardName
+{
+	public:
+		wxString BoardName;
+		wxString BoardURL;
+		wxString BoardNameAscii;
+};
+
+#endif /* DATATYPE_H_ */
