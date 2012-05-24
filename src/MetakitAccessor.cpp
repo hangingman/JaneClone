b9df9e73ed121a9e9fa10da3eca8f46d15e1abf9
Hiroyuki Nagata <newserver002@gmail.com>
2012-05-25 01:35:42 +0900

    ・メニューバー部分を新たに設定した ・ライセンスをGPLv2に設定した

--- src/MetakitAccessor.cpp
+++ src/MetakitAccessor.cpp
@@ -1,8 +1,22 @@
-/*
- * MetakitAccessor.cpp
+/* JaneClone - a text board site viewer for 2ch
+ * Copyright (C) 2012 Hiroyuki Nagata
  *
- *  Created on: 2012/05/13
- *      Author: learning
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contributor:
+ *	Hiroyuki Nagata <newserver002@gmail.com>
  */
 
 #include "MetakitAccessor.h"
