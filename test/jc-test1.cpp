dc5b5326e49a2b93af56c302dc3377e4a75ca5ab
hiroyuki nagata <newserver002@gmail.com>
2013-07-18 23:55:40 +0900

    ・テストコード修正

--- test/jc-test1.cpp
+++ test/jc-test1.cpp
@@ -6,6 +6,10 @@ class JaneCloneTest1 : public wxAppConsole {
 public:
      virtual bool OnInit();
      virtual int  OnRun();
+
+private:
+     int Test1();
+     int Test2();
 };
 
 IMPLEMENT_APP(JaneCloneTest1)
@@ -16,12 +20,47 @@ bool JaneCloneTest1::OnInit() {
 
 int JaneCloneTest1::OnRun() {
 
+     Test1();
+     Test2();
+
+     return 0;
+}
+
+int JaneCloneTest1::Test1() {
+
      wxString test1 = wxT("あいうえお");
 
      const wxString option = wxT("--ic=UTF-8 --oc=CP932");
      wxNKF* nkf = new wxNKF();
-     const std::string std1 = nkf->WxToMultiByte(test1, option);
-     std::cout << std1 << std::endl;
+     const std::string std = nkf->WxToMultiByte(test1, option);
+     delete nkf;
 
-     return 0;
+     std::cout.setf(std::ios::hex, std::ios::basefield);
+     std::cout.setf(std::ios::showbase);
+
+     for (std::string::const_iterator it = std.begin(); it != std.end(); ++it) {
+	  std::cout << (int)(*it & 0xff) << " ";
+     }
+
+     std::cout << std::endl;
 }
+
+int JaneCloneTest1::Test2() {
+
+     wxString test2 = wxT(":test:あいうえお");
+
+     const wxString option = wxT("--ic=UTF-8 --oc=CP932");
+     wxNKF* nkf = new wxNKF();
+     const std::string std = nkf->WxToMultiByte(test2, option);
+     delete nkf;
+
+     std::cout.setf(std::ios::hex, std::ios::basefield);
+     std::cout.setf(std::ios::showbase);
+
+     for (std::string::const_iterator it = std.begin(); it != std.end(); ++it) {
+	  std::cout << (int)(*it & 0xff) << " ";
+     }
+
+     std::cout << std::endl;
+}
+
