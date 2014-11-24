24c43d18c591da2deb897b04bbcc8ee314701fa3
hiroyuki nagata <hiroyuki@vyatta-maiden.hm.moe>
2014-11-25 00:08:52 +0900

    Update upload_bintray.sh

--- misc/upload_bintray.sh
+++ misc/upload_bintray.sh
@@ -18,18 +18,43 @@
 VERSION=`cat ../configure.ac | grep AC_INIT | awk '{print $2}' | sed -e 's/\[\|\]\|,//g'`
 echo "JaneClone version is ${VERSION}"
 
-FILENAME="janeclone_${VERSION}_amd64.deb"
-echo "File name is ${FILENAME}"
+# Package
+DEB_FILENAME="janeclone_${VERSION}_amd64.deb"
 
-if ! test -e ../${FILENAME}; then
-    echo "${FILENAME} is not created abort...";
+# .dsc file
+DSC_FILENAME="janeclone_${VERSION}.dsc"
+# source file
+SRC_FILENAME="janeclone_${VERSION}.tar.gz"
+# other files
+BLD_FILENAME="janeclone_${VERSION}_amd64.build"
+CHG_FILENAME="janeclone_${VERSION}_amd64.changes"
+
+echo "File name is ${DEB_FILENAME}"
+
+if ! test -e ../${DEB_FILENAME}; then
+    echo "${DEB_FILENAME} is not created abort...";
     exit 1;
 fi
 
 CURL="curl -v --user hiroyuki-nagata:${BINTRAY_TOKEN}"
 METHOD="-X PUT --data-binary @- "
 API="https://api.bintray.com"
-RESOURCE="/content/hiroyuki-nagata/deb/JaneClone/${VERSION}/${FILENAME}?override=1?publish=1"
 
+#
 # Here upload executing !
-cat ../${FILENAME} | ${CURL} ${METHOD} ${API}${RESOURCE}
+#
+
+# .deb file
+RESOURCE="/content/hiroyuki-nagata/deb/JaneClone/${VERSION}/${DEB_FILENAME};publish=1?override=1"
+cat ../${DEB_FILENAME} | ${CURL} ${METHOD} ${API}${RESOURCE}
+# .dsc file
+RESOURCE="/content/hiroyuki-nagata/deb/JaneClone/${VERSION}/${DSC_FILENAME};publish=1?override=1"
+cat ../${DSC_FILENAME} | ${CURL} ${METHOD} ${API}${RESOURCE}
+# source file
+RESOURCE="/content/hiroyuki-nagata/deb/JaneClone/${VERSION}/${SRC_FILENAME};publish=1?override=1"
+cat ../${SRC_FILENAME} | ${CURL} ${METHOD} ${API}${RESOURCE}
+# other files
+RESOURCE="/content/hiroyuki-nagata/deb/JaneClone/${VERSION}/${BLD_FILENAME};publish=1?override=1"
+cat ../${BLD_FILENAME} | ${CURL} ${METHOD} ${API}${RESOURCE}
+RESOURCE="/content/hiroyuki-nagata/deb/JaneClone/${VERSION}/${CHG_FILENAME};publish=1?override=1"
+cat ../${CHG_FILENAME} | ${CURL} ${METHOD} ${API}${RESOURCE}
